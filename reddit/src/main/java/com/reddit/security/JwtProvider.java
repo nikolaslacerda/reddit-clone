e97bf10d6173bc2f5d927e9b977dca9fdf76d2b9
nikolaslacerda <nikolas.lacerda@hotmail.com>
2020-09-14 21:05:35 -0300

    add jwt validation

--- reddit/src/main/java/com/reddit/security/JwtProvider.java
+++ reddit/src/main/java/com/reddit/security/JwtProvider.java
@@ -6,6 +6,7 @@ import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
 
@@ -17,8 +18,8 @@ import org.springframework.stereotype.Service;
 
 import com.reddit.exceptions.SpringRedditException;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-
 
 @Service
 public class JwtProvider {
@@ -47,6 +48,24 @@ public class JwtProvider {
 		} catch (KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
 			throw new SpringRedditException("Exception occured while retrieving public key from keystore");
 		}
+	}
+
+	public boolean validateToken(String jwt) {
+		Jwts.parser().setSigningKey(getPublicKey()).parseClaimsJws(jwt);
+		return true;
+	}
+
+	private PublicKey getPublicKey() {
+		try {
+			return keyStore.getCertificate("springblog").getPublicKey();
+		} catch (KeyStoreException e) {
+			throw new SpringRedditException("Exception occured while retrieving public key from keystore");
+		}
+	}
+	
+	public String getUsernameFromJwt(String token) {
+		Claims claims = Jwts.parser().setSigningKey(getPublicKey()).parseClaimsJws(token).getBody();
+		return claims.getSubject();
 	}
 
 }
